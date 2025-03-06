1b75e9a6b7877caef3ce111bfb25deecfd48dd06
riana128 <ririsa0812@gmail.com>
2025-03-06 09:02:53 +0700

    landing

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/product-left-sidebar', function () {
+    return view('product-left-sidebar');
+})->name('product.left.sidebar');
+
+
+
 require __DIR__.'/auth.php';
